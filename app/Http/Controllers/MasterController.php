0765c461108f166e511dff80d6cfac59cee1e259
mkhamdank <gsyn153@gmail.com>
2023-09-01 09:05:29 +0700

    MK -  api 5

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -38,7 +38,7 @@ class MasterController extends Controller
         $outgoing_crestec = DB::table('qa_outgoing_vendor_crestecs')->where('synced_at',null)->get();
         $outgoing = DB::table('qa_outgoing_vendors')->where('synced_at',null)->get();
         // $outgoing_final = DB::table('qa_outgoing_vendor_finals')->where('synced_at',null)->get();
-        // $outgoing_recheck = DB::table('qa_outgoing_vendor_rechecks')->where('synced_at',null)->get();
+        $outgoing_recheck = DB::table('qa_outgoing_vendor_rechecks')->where('synced_at',null)->get();
 
         $response = array(
             'status' => true,
@@ -46,7 +46,7 @@ class MasterController extends Controller
             'outgoing_crestec' => $outgoing_crestec,
             'outgoing' => $outgoing,
             // 'outgoing_final' => $outgoing_final,
-            // 'outgoing_recheck' => $outgoing_recheck,
+            'outgoing_recheck' => $outgoing_recheck,
             'sync_at' => date('Y-m-d H:i:s'),
         );
         return Response::json($response);
