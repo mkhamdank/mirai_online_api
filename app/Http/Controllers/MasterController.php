d502f14e290019debe86fd41a586983fdb4b9183
DESKTOP-MIS10\YMPI <fakhrizal.ihza.mahendra@music.yamaha.com>
2025-05-19 10:37:34 +0700

    MK - driver phone

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -547,6 +547,7 @@ class MasterController extends Controller
                     'requested_id' => $email[0]['requested_id'],
                     'requested_name' => $email[0]['requested_name'],
                     'requested_phone' => $email[0]['requested_phone'],
+                    'driver_phone' => $email[0]['driver_phone'],
                     'created_by_id' => $email[0]['created_by_id'],
                     'created_by_name' => $email[0]['created_by_name'],
                     'created_at' => $email[0]['created_at'],
